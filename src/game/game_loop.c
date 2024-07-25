26b28619680ffa786f858416d3e136a7088b8957
Nestor Cruz Gambero <ncruz-ga@c3r10s2.42malaga.com>
2024-07-25 12:12:28 +0200

    Añadido comentarios

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 11:59:56 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/07/25 12:09:36 by ncruz-ga         ###   ########.fr       */
+/*   Updated: 2024/07/25 12:12:03 by ncruz-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	init_game(t_data *data)
 	data->mlx = mlx_init(S_WIDTH, S_HEIGHT, "cub3D", false);
 	if (!data->mlx)
 		return (1);
+	mlx_loop_hook(data->mlx, /*funcion que recibe el raycast loop*/, data);
+	mlx_key_hook(data->mlx, /*funcion de las teclas*/, data);
 	mlx_loop(data->mlx);
 	mlx_terminate(data->mlx);
 	return (0);
