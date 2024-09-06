3fff4a8cb4fdbf65349d0a5d1496c1751c90d2c5
Ester Castellano Rios <escastel@c2r8s4.42malaga.com>
2024-09-06 16:42:20 +0200

    movimiento con raton

--- src_bonus/game_bonus/hook_bonus.c
+++ src_bonus/game_bonus/hook_bonus.c
@@ -6,7 +6,7 @@
 /*   By: escastel <escastel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:44:23 by ncruz-ga          #+#    #+#             */
-/*   Updated: 2024/09/06 16:00:28 by escastel         ###   ########.fr       */
+/*   Updated: 2024/09/06 16:37:31 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,14 @@ static void	clean_wall(t_data *data)
 	}
 }*/                
 
+static void	move_camara_mouse(t_data *data)
+{
+	mlx_set_cursor_mode(data->mlx, 0x00034002);
+	mlx_get_mouse_pos(data->mlx, &data->m_x, &data->m_y);
+	data->p_angle += correct_angle(((data->m_x - (S_WIDTH / 2)) / 50) * (M_PI / 180));
+	mlx_set_mouse_pos(data->mlx, S_WIDTH / 2, S_HEIGHT / 2);	
+}
+
 void	hook(void *param)
 {
 	t_data	*data;
@@ -41,4 +49,5 @@ void	hook(void *param)
 	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(data->mlx);
 	player_move(data);
+	move_camara_mouse(data);
 }
